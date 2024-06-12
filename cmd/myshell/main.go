8031b87fee1fc74100786026f52902be8fd4b2ef
6a10 <2nfymjxjg@mozmail.com>
2024-06-13 01:05:14 +0200

    test

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,9 +4,9 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"os/signal"
+	_ "os/signal"
 	"strings"
-	"syscall"
+	_ "syscall"
 )
 
 type Command interface{}
@@ -21,31 +21,37 @@ func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	// fmt.Println("Logs from your program will appear here!")
 
+	reader := bufio.NewReader(os.Stdin)
+
 	for {
 		// Uncomment this block to pass the first stage
 		fmt.Fprint(os.Stdout, "$ ")
 
 		// Wait for user input
-		cmdString, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		cmdString, err := reader.ReadString('\n')
 		if err != nil {
 			fmt.Fprint(os.Stderr, "error reading command")
 			os.Exit(1)
 		}
 
-		words := strings.Split(cmdString, " ")
+		words := strings.Split(cmdString[:len(cmdString)-1], " ")
+		if len(words) == 0 {
+			continue
+		}
 		if _, ok := commands[words[0]]; ok {
 		} else {
-			fmt.Fprintf(os.Stdout, "%s: command not found", words[0])
+			fmt.Fprintf(os.Stdout, "%s: command not found\n", words[0])
+			os.Exit(1)
 		}
 	}
 }
 
-func init() {
-	c := make(chan os.Signal)
-	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
-	go func() {
-		<-c
-		// Run Cleanup
-		os.Exit(1)
-	}()
-}
+// func init() {
+// 	c := make(chan os.Signal)
+// 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+// 	go func() {
+// 		<-c
+// 		// Run Cleanup
+// 		os.Exit(1)
+// 	}()
+// }
