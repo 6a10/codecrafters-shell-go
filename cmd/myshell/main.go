1f67e39fb02ed781ad36b0a6ae81b89bccd2f9ad
6a10 <2nfymjxjg@mozmail.com>
2024-06-13 00:29:16 +0200

    test

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,15 +4,48 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"os/signal"
+	"strings"
+	"syscall"
 )
+
+type Command interface{}
+
+type Cmd struct{}
+
+var commands = map[string]Command{"echo": Cmd{}, "cd": Cmd{}}
+
+// var controls = map[string]error{"^C": fmt.Errorf("exit command"), "^D": fmt.Errorf("close command")}
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	// fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment this block to pass the first stage
-	fmt.Fprint(os.Stdout, "$ ")
+	for {
+		// Uncomment this block to pass the first stage
+		fmt.Fprint(os.Stdout, "$ ")
 
-	// Wait for user input
-	bufio.NewReader(os.Stdin).ReadString('\n')
+		// Wait for user input
+		cmdString, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			fmt.Fprint(os.Stderr, "error reading command")
+			os.Exit(1)
+		}
+
+		words := strings.Split(cmdString, " ")
+		if _, ok := commands[words[0]]; ok {
+		} else {
+			fmt.Fprintf(os.Stdout, "%s: command not found", words[0])
+		}
+	}
+}
+
+func init() {
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		// Run Cleanup
+		os.Exit(1)
+	}()
 }
